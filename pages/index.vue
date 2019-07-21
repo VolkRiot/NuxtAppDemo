c92ab0f4a7fc7d0973da9555768dc37bccac94b0
Mikhail Metrikin <metrikin@gmail.com>
2019-07-21 16:38:15 -0700

    Rendering example nuxt app with components

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,45 @@
 <template>
-  <h1>Hello world testing</h1>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <TestComponent 
+      id="passed-id"
+      myClass="passed-class"
+      msg="TESING ONE TIME BIND" 
+      rawHtml="<h1>This h1 is intrepreted HTML</h1>" 
+      count="0"
+      />
+  </div>
 </template>
+
+<script>
+
+import TestComponent from '../components/TestComponent'
+
+export default {
+  name: 'app',
+  components: {
+    TestComponent
+  },
+  data() {
+    return {
+      test: true
+    }
+  },
+  methods: {
+    toggleTest() {
+      this.test = !this.test;
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
