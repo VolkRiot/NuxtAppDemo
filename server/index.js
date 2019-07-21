b81fbe1d7e7173373641f0039cdbe5e6b9747818
Mikhail Metrikin <metrikin@gmail.com>
2019-07-21 13:48:03 -0700

    Added Koa server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,12 @@
 import 'dotenv/config';
+import Koa from 'koa';
 
-console.log(process.env.TEST_VARIABLE);
+// console.log(process.env.TEST_VARIABLE);
+
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = "Hello World! Koa";
+})
+
+app.listen(3000);
